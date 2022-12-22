87d38d32b3f20f54ebab13bedcdd7c175f127c4d
geysha1337 <114612160+geysha1337@users.noreply.github.com>
2022-12-22 14:07:40 +0300

    Add files via upload

---
+++ bpm_22_2_1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int main()
+{
+    for (double x = 0.1; x <= 1.05; x = x + 0.05) {
+        double sum = 0;
+        int i = 0;
+        bool flag = true;
+        while (flag == true) {
+            double dx = (pow(-1, i) * pow(x, (2 * i) + 1)) / ((2 * i) + 1);
+            if (abs(dx) < 0.0001) {
+                flag = false;
+            }
+            else {
+                sum += dx;
+            }
+            i += 1;
+        }
+        std::cout << x << " " << sum << "\n";
+    }
+}
